e8926541012d37a8164a81f1e29c2ae3d90b8db2
Lyu Chenggong <lcgong@tiangong.edu.cn>
2024-03-10 10:38:10 +0800

    修改版本号0.3.2

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
-
 use winres;
 
 fn main() {
     if cfg!(target_os = "windows") {
-      let mut res = winres::WindowsResource::new();
-      res.set_icon(r#"resources\tgba-jupyterlab-48x48.ico"#);
-      res.compile().unwrap();
+        let mut res = winres::WindowsResource::new();
+        res.set_icon(r#"resources\tgba-jupyterlab-48x48.ico"#);
+        res.compile().unwrap();
     }
-  }
+}
